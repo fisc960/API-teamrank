506488210e97050c404d1bc57b694caa836c772e
fisc960 <poplo.fisch@gmail.com>
2025-12-08 20:54:15 -0500

    12/8 #15 push

---
+++ Migrations/20251209015327_FixPostgreSQLTableNames.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GemachApp.Migrations
+{
+    public partial class FixPostgreSQLTableNames : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
